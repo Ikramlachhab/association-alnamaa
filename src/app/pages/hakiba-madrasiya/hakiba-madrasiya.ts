dfacbdb3528ab8ed1fa3584a47871a6681322331
oumaima <rbiboumaim2004@email.com>
2026-02-01 08:43:21 +0100

    Fix routing for hakiba-madrasiya

---
+++ src/app/pages/hakiba-madrasiya/hakiba-madrasiya.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-hakiba-madrasiya',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './hakiba-madrasiya.html',
+  styleUrl: './hakiba-madrasiya.css'
+})
+export class HakibaMadrasiyaComponent {
+  // Logic dial l-7aqiba ghadi n-zidouh men be3d
+}
