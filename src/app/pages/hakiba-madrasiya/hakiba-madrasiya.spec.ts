4b2ef656f92aa3a3714d95190cd4bed54291bb22
rbiboumaima <rbiboumaima2004@gmail.com>
2026-02-07 15:27:43 +0100

    Update hakiba-madrasiya.spec.ts

--- src/app/pages/hakiba-madrasiya/hakiba-madrasiya.spec.ts
+++ src/app/pages/hakiba-madrasiya/hakiba-madrasiya.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HakibaMadrasiyaComponent } from './hakiba-madrasiya';
+import { HakibaMadrasiyaComponent } from './hakiba-madrasiya.component';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 describe('HakibaMadrasiyaComponent', () => {
   let component: HakibaMadrasiyaComponent;
@@ -8,15 +9,21 @@ describe('HakibaMadrasiyaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HakibaMadrasiyaComponent, FormsModule]
+      imports: [HakibaMadrasiyaComponent, CommonModule, FormsModule]
     }).compileComponents();
-
     fixture = TestBed.createComponent(HakibaMadrasiyaComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create the component', () => {
+  it('يجب إنشاء المكون بنجاح وتحقق من مرونة رقم الهاتف الدولي', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('يجب التحقق من عمل نظام التنبيهات الخارجي', () => {
+    component.donationAmount = 10;
+    component.confirmDonation();
+    expect(component.toastActive).toBeTrue();
+    expect(component.toastType).toBe('error');
   });
 });
